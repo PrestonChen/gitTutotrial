de627c63db7facd0af2a36b9e5966592e84b3d4d
Preston Chen <tianpei.chen@mail.mcgill.ca>
2017-07-10 00:16:30 -0400

    initial commit

---
+++ test_project/test_project/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  test_project
+//
+//  Created by Tianpei Chen on 10/07/2017.
+//  Copyright © 2017 Tianpei Chen. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("this is my first C program on Mac!\n");
+    return 0;
+}
